3aac407f17086b59606d547a39a7a4e3e1395090
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2022-11-08 20:42:10 +0100

    Created bohr2aa and updated regex for tinker_xyz

--- bohr2aa.py
+++ bohr2aa.py
@@ -4,11 +4,9 @@
 """
 Author: Martin Dagleish (MRJD)
 
-Version 0.3.3
+Version 0.1.0
 
-This script converts the Chem3D Tinker xyz files to standard xyz files.
-This is needed in order to use the .xyz files for any other program.
-Tinker xyz files have many useless numbers and lines that are not needed.
+This script converts xyz coordinates in Bohr units to AA (Angstrom). 
 
 MIT License
 
@@ -34,29 +32,45 @@ SOFTWARE.
 """
 
 # * Changelog:
-# * 0.3.3 - Fixed regex pattern for rare cases where there is nothing after last z-coordinate.
-# * 0.3.2 - Fixed the replace implementation and move old_std when new is created.
-# * 0.3.1 - Fixed bug for coordinates with double digits
-# * 0.3.0 - Simplified the code (unnecessary if statements removed) and prettified the print statements.
-# * 0.2.0 - Added fix for negative values for the number of atoms and renewed the regex pattern.
 # * 0.1.0 - Initial release
 
 import os
 import sys
 
 try:
-    import re
+    import regex as re 
 except ImportError:
-    print("Please install re. (regular expressions for Python)")
+    print("Please install regex. (regular expressions for Python)")
     sys.exit(1)
 
 #! https://regex101.com/r/1U1YsC/1 -> updated Regex Pattern
 pattern = r"\b\w{1,2}\s+((\-|\+)?\d+[,.]\d+\s*){3,3}"  # * independent of \n
-
-# * Old patterns:
-# lp_pattern = r"\bLp\s+((-| )\d[,.]\d+\s+){3,3}" # ! match the pattern for Lp -> https://regex101.com/r/Wt0mek/1
 regex = re.compile(pattern)
-VERSION = "0.3.3"
+VERSION = "0.1.0"
+
+bohr2aa = 0.529177210903
+
+def convert_zvals_to_symbols(z_val):
+    return atom_dict.get(z_val,z_val) # * If failed then return z_val (int)
+
+atom_dict = {
+        1: "H",    2: "He",
+        3: "Li",   4: "Be",  5: "B",   6: "C",   7: "N",   8: "O",   9: "F",   10: "Ne",
+        11: "Na", 12: "Mg", 13: "Al", 14: "Si", 15: "P",  16: "S",  17: "Cl",  18: "Ar",
+        19: "K",  20: "Ca", 
+            21: "Sc", 22: "Ti", 23: "V",  24: "Cr", 25: "Mn",  26: "Fe", 27: "Co", 28: "Ni", 29: "Cu", 30: "Zn", 
+                            31: "Ga", 32: "Ge", 33: "As", 34: "Se", 35: "Br",  36: "Kr",
+        37: "Rb", 38: "Sr", 
+            39: "Y",  40: "Zr", 41: "Nb", 42: "Mo", 43: "Tc",  44: "Ru", 45: "Rh", 46: "Pd", 47: "Ag", 48: "Cd",
+                            49: "In", 50: "Sn", 51: "Sb", 52: "Te", 53: "I",   54: "Xe",
+        55: "Cs", 56: "Ba",
+            57: "La", 58: "Ce", 59: "Pr", 60: "Nd", 61: "Pm", 62: "Sm", 63: "Eu", 64: "Gd", 65: "Tb", 66: "Dy", 67: "Ho", 68: "Er", 69: "Tm", 70: "Yb", 71: "Lu",
+            72: "Hf", 73: "Ta", 74: "W",  75: "Re", 76: "Os", 77: "Ir", 78: "Pt", 79: "Au", 80: "Hg",
+                            81: "Tl", 82: "Pb", 83: "Bi", 84: "Po", 85: "At", 86: "Rn",
+        87: "Fr", 88: "Ra",
+            89: "Ac", 90: "Th", 91: "Pa", 92: "U",  93: "Np", 94: "Pu", 95: "Am", 96: "Cm", 97: "Bk", 98: "Cf", 99: "Es", 100: "Fm", 101: "Md", 102: "No", 103: "Lr",
+            104: "Rf", 105: "Db", 106: "Sg", 107: "Bh", 108: "Hs", 109: "Mt", 110: "Ds", 111: "Rg", 112: "Cn", 113: "Nh", 114: "Fl", 115: "Mc", 116: "Lv", 117: "Ts", 118: "Og"
+    }
 
 def main():
     """
@@ -89,12 +103,24 @@ def main():
             for line in lines:
                 match = re.search(regex, line)
                 if match:
-                    match = match.group(0).strip()
-
                     if "Lp" in line:
                         continue
 
-                    f.write(match + "\n")
+                    match = match.group(0).strip()
+                    match_spl = match.split(" ")
+                    match_spl[:] = [x.strip() for x in match_spl if x]
+                    atom, coordx, coordy, coordz = match_spl
+
+                    if atom.isnumeric():
+                        atom = int(atom)
+                        atom = convert_zvals_to_symbols(atom)
+                        
+                    coordx = f"{float(coordx) * bohr2aa:>10.10f}"
+                    coordy = f"{float(coordy) * bohr2aa:>10.10f}"
+                    coordz = f"{float(coordz) * bohr2aa:>10.10f}"
+                    new_line = atom + " " + coordx + " " + coordy + " " + coordz 
+
+                    f.write(new_line + "\n")
 
         print(
             f"\n  Export of {filename + ext} as {new_filename} finished."
