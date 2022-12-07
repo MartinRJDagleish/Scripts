3f0434d926fea764c9e536e887cee132d47e3e8c
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2022-12-07 20:30:39 +0100

    Version 0.3.2

--- run_orca.py
+++ run_orca.py
@@ -4,7 +4,7 @@
 """
 Author: Martin Dagleish (MRJD)
 
-Version 0.2.5
+Version 0.3.2
 
 This is a wrapper script for the ORCA programme.
 
@@ -32,15 +32,18 @@ SOFTWARE.
 """
 
 # * Changelog
-# * 0.2.5 - Added more output to cwd (.out, .hess and .xyz files) 
-# * 0.2.4 - Switched os.system for subprocess.run in script and updated Windows 
+# * 0.3.2 - Fixed bug with .hess file not being copied from frequency calculations
+# * 0.3.1 - Added check for .hess file from frequency calculations
+# * 0.3.0 - Simplified code and reduced checks needed for OS (Windows or Linux)
+# * 0.2.5 - Added more output to cwd (.out, .hess and .xyz files)
+# * 0.2.4 - Switched os.system for subprocess.run in script and updated Windows
 # * 0.2.3 - Added option to copy multiple xyz files to temp1 if wanted.
 # * 0.2.2 - Prettified printing of ORCA output.
 # * 0.2.1 - Path slashes wrong for Linux
 # * 0.2.0 - Fixed Linux not working as stdout has to be written via python and not ">"
 # * 0.1.0 - Initial release
 
-VERSION = "0.2.5"
+VERSION = "0.3.2"
 
 import os
 import sys
@@ -58,15 +61,18 @@ except ImportError:
 
 # * Show the OS running this script
 OPERATING_SYTEM = sys.platform
+WIN_OS_BOOL = (
+    False if sys.platform in ("darwin", "linux", "linux2") else True
+)  # * Check if OS is Windows
 
-#! NOTE: 
+#! NOTE:
 # * This is a HIGHLY custom directory for the ORCA programme, but the problem is
 # * that Linux already has a 'ORCA' programme (screen reader) which means that
 # * the path has to be added manually. (Or at least I don't know how else to do it)
-if OPERATING_SYTEM in ("linux", "linux2"):
-    ORCA_PATH = "/loctmp/dam63759/orca/orca"
-elif OPERATING_SYTEM == "win32":
+if WIN_OS_BOOL:
     ORCA_PATH = r"C:\ORCA\orca.exe"
+else:
+    ORCA_PATH = "/loctmp/dam63759/orca/orca"
 
 # * Create the parser
 orca_parser = argparse.ArgumentParser(
@@ -107,69 +113,54 @@ args = orca_parser.parse_args()
 if __name__ == "__main__":
     cwd = os.getcwd()
 
-    input_file = args.input_file
-    if not input_file.endswith(".inp"):
-        input_file += ".inp"
-
+    input_file = (
+        args.input_file
+        if args.input_file.endswith(".inp")
+        else args.input_file + ".inp"
+    )
     namespace = os.path.splitext(input_file)[0]
 
+    #* Check the type of job for copying .hess file:
+    HESS_FILE = False
+    with open(input_file, "r", encoding="utf-8") as f:
+        for l_no, line in enumerate(f):
+            if 'freq' in line.lower():
+                HESS_FILE = True
+                break
+
+            
     # * mkdir temp1 folder for xtb files
     temp1_path = os.path.join(cwd, "temp1")
     if not os.path.isdir(temp1_path):
         os.mkdir(temp1_path)
 
     # * copy .xyz file to temp1 folder
-    if OPERATING_SYTEM == "win32":
-        subprocess.run(
-            ["copy", input_file, temp1_path], stdout=subprocess.PIPE, check=True, shell=True
-        )
-        if args.xyz:
-            xyz_files = [
-                xyz if xyz.endswith(".xyz") else xyz + ".xyz" for xyz in args.xyz
-            ]
-            for xyz_file in xyz_files:
-                if os.path.isfile(os.path.join(cwd, xyz_file)):
-                    subprocess.run(
-                        ["copy", xyz_file, temp1_path],
-                        stdout=subprocess.PIPE,
-                        check=True,
-                        shell=True
-                    )
-                else:
-                    print(f"{xyz_file} not found in {cwd}")
-                    sys.exit(1)
+    if WIN_OS_BOOL:
+        copy_cmd = "copy"
     else:
-        subprocess.run(
-            ["cp", input_file, temp1_path], stdout=subprocess.PIPE, check=True
-        )
-        if args.xyz:
-            xyz_files = [
-                xyz if xyz.endswith(".xyz") else xyz + ".xyz" for xyz in args.xyz
-            ]
-            for xyz_file in xyz_files:
-                if os.path.isfile(os.path.join(cwd, xyz_file)):
-                    subprocess.run(
-                        ["cp", xyz_file, temp1_path], stdout=subprocess.PIPE, check=True
-                    )
-                else:
-                    print(f"{xyz_file} not found in {cwd}")
-                    sys.exit(1)
+        copy_cmd = "cp"
+
+    subprocess.run([copy_cmd, input_file, temp1_path], check=True, shell=WIN_OS_BOOL)
+
+    if args.xyz:
+        xyz_files = [xyz if xyz.endswith(".xyz") else xyz + ".xyz" for xyz in args.xyz]
+        for xyz_file in xyz_files:
+            if os.path.isfile(os.path.join(cwd, xyz_file)):
+                subprocess.run(
+                    [copy_cmd, xyz_file, temp1_path], check=True, shell=WIN_OS_BOOL
+                )
+            else:
+                print(f"{xyz_file} not found in {cwd}")
+                sys.exit(1)
 
     os.chdir(temp1_path)
 
     # *---------------------------------#
     # * Run the calculation
-    if OPERATING_SYTEM in ("win32", "Windows"):
+    with open(f"{namespace}.out", "w", encoding="utf-8") as out:
         subprocess.run(
-            # ["start", "/b", f"{ORCA_PATH}", input_file, ">", f"{namespace}.out"],
-            [f"{ORCA_PATH}", input_file, ">", f"{namespace}.out"],
-            stdout=subprocess.PIPE,
-            shell=True,
-            check=True
+            [f"{ORCA_PATH}", input_file], stdout=out, check=True, shell=WIN_OS_BOOL
         )
-    elif OPERATING_SYTEM in ("linux", "Linux", "Darwin"):
-        with open(f"{namespace}.out", "w", encoding="utf-8") as out:
-            subprocess.run([f"{ORCA_PATH}", input_file], stdout=out, check=True)
     # *---------------------------------#
 
     print("\n" + 40 * "-")
@@ -177,42 +168,20 @@ if __name__ == "__main__":
     print(40 * "-")
 
     os.chdir("..")
-    if OPERATING_SYTEM in ("win32", "Windows"):
-        copy_cmds = [
-            ("copy " + f"{temp1_path}\\{namespace}.{ext} " + cwd).split()
-            for ext in (
-                "out", 
-                "hess", 
-                "xyz"
-            )
-        ]
-        for cmd in copy_cmds:
-            subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True)
-        # * Previously
-        # subprocess.run(
-        #     ["copy", f"{temp1_path}\\{namespace}.out", cwd],
-        #     stdout=subprocess.PIPE,
-        #     shell=True,
-        #     check=True
-        # )
-    elif OPERATING_SYTEM in ("linux", "Linux", "Darwin"):
-        copy_cmds = [
-            ("cp " + f"{temp1_path}/{namespace}.{ext} " + cwd).split()
-            for ext in (
-                "out", 
-                "hess", 
-                "xyz"
-            )
-        ]
-        for cmd in copy_cmds:
-            subprocess.run(cmd, stdout=subprocess.PIPE, shell=True, check=True)
-        #* Previously
-        # subprocess.run(
-        #     ["cp", f"{temp1_path}/{namespace}.out", cwd],
-        #     stdout=subprocess.PIPE,
-        #     check=True
-        # )
+
+    extensions = ["out", "xyz"]
+    if HESS_FILE:
+        extensions.append("hess")
+
+    copy_cmds = [
+        (f"{copy_cmd} " + f"{temp1_path}/{namespace}.{ext} " + cwd).split()
+        for ext in extensions
+    ]
+
+    for cmd in copy_cmds:
+        subprocess.run(cmd, shell=WIN_OS_BOOL, check=True)
 
     print(40 * "-")
     print("*" + "Output copied to CWD".center(38, " ") + "*")
     print(40 * "-")
+    sys.exit(0)
