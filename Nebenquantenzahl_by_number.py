46ad0f44f79418657212c0741a29db12c878489a
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2023-06-19 12:29:57 +0200

    Formatting; 2023-06-19 12:29:46

--- Nebenquantenzahl_by_number.py
+++ Nebenquantenzahl_by_number.py
@@ -2,16 +2,15 @@
 
 import string
 
-
 def stringType(L):
-    #* List for all letters
-    #* Get index of 'f' 
+    # * List for all letters
+    # * Get index of 'f'
     alphabet_list = list(string.ascii_lowercase)
-    f_index = alphabet_list.index('f') 
+    f_index = alphabet_list.index("f")
     l_dict = {
-        num : l 
-        for num, l in 
-        zip(range(13),
-            ['s', 'p', 'd'] + alphabet_list[f_index:f_index+10])
-        }            
-    return l_dict.get(L) 
+        num: l
+        for num, l in zip(
+            range(13), ["s", "p", "d"] + alphabet_list[f_index : f_index + 10]
+        )
+    }
+    return l_dict.get(L)
